88cae21321f887787676cd04082fdda7d72cd385
Divesh Kumavat <diveshkumavat@gmail.com>
2026-06-28 14:26:17 +0530

    Time: 0 ms (100%), Space: 15 MB (63.94%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -19,11 +19,10 @@ public:
         q.push(root);
         while(!q.empty()){
             int size=q.size();
-            stack<int> st;
             for(int i=0;i<size;i++){
                 TreeNode* node=q.front();
                 q.pop();
-                st.push(node->val);
+                if(i==size-1) ans.push_back(node->val);
                 if(node->left!=nullptr){
                     q.push(node->left);
                 }
@@ -31,7 +30,6 @@ public:
                     q.push(node->right);
                 }
             }
-            ans.push_back(st.top());
         }
         return ans;
     }
