813c95f748e27699c9278941a686e12f1988a223
Divesh Kumavat <diveshkumavat@gmail.com>
2026-06-28 14:04:22 +0530

    Time: 4 ms (22.4%), Space: 16.2 MB (53.26%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,33 +11,58 @@
  */
 class Solution {
 public:
-    void storeTuples(TreeNode* root,vector<tuple<int,int,int>>& tuples,int row,int col){
-        if(root==NULL) return;
-        tuples.push_back({col,row,root->val});
-        storeTuples(root->left,tuples,row+1,col-1);
-        storeTuples(root->right,tuples,row+1,col+1);
+    // void storeTuples(TreeNode* root,vector<tuple<int,int,int>>& tuples,int row,int col){
+    //     if(root==NULL) return;
+    //     tuples.push_back({col,row,root->val});
+    //     storeTuples(root->left,tuples,row+1,col-1);
+    //     storeTuples(root->right,tuples,row+1,col+1);
+    // }
+
+    void f(TreeNode* root,map<int,vector<pair<int,int>>>& mp,int row,int col){
+        if(root==nullptr) return;
+        mp[col].push_back({row,root->val});
+        f(root->left,mp,row+1,col-1);
+        f(root->right,mp,row+1,col+1);
     }
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        vector<tuple<int,int,int>> tuples;
-        storeTuples(root,tuples,0,0);
-        if(tuples.empty()) return {};
+        //tuples solution
 
-        sort(tuples.begin(),tuples.end());
+        // vector<tuple<int,int,int>> tuples;
+        // storeTuples(root,tuples,0,0);
+        // if(tuples.empty()) return {};
 
+        // sort(tuples.begin(),tuples.end());
+
+        // vector<vector<int>> ans;
+        // int n=tuples.size();
+        // int i=0;
+        // int column=get<0>(tuples[0]);
+        // while(i<n){
+        //     vector<int> row;
+        //     while(i<n && get<0>(tuples[i])==column){
+        //         row.push_back(get<2>(tuples[i]));
+        //         i++;
+        //     }
+        //     ans.push_back(row);
+        //     if(i<n) column=get<0>(tuples[i]);
+        // }
+
+        // return ans;
+
+        // map solution
+        map<int,vector<pair<int,int>>> mp;
+        f(root,mp,0,0);
         vector<vector<int>> ans;
-        int n=tuples.size();
-        int i=0;
-        int column=get<0>(tuples[0]);
-        while(i<n){
-            vector<int> row;
-            while(i<n && get<0>(tuples[i])==column){
-                row.push_back(get<2>(tuples[i]));
-                i++;
-            }
-            ans.push_back(row);
-            if(i<n) column=get<0>(tuples[i]);
-        }
 
+        for(auto it:mp){
+            sort(it.second.begin(),it.second.end());
+            int size=it.second.size();
+            vector<int> col;
+            for(int i=0;i<size;i++){
+                col.push_back(it.second[i].second);
+            }
+            ans.push_back(col);
+        }
         return ans;
     }
 };
