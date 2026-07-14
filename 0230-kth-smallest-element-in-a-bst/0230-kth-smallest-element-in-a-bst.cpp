d769facc506a6be74cae3e23f057e5708b475403
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-14 13:31:57 +0530

    Time: 2 ms (9.41%), Space: 24.2 MB (90.38%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -12,19 +12,50 @@
 class Solution {
 public:
     int count=0;
-    void inorder(TreeNode* root,int k,int &ans){
-        if(!root || count>=k) return;
-        inorder(root->left,k,ans);
-        count++;
-        if(count==k){
-            ans=root->val;
-            return;
-        }
-        inorder(root->right,k,ans);
-    }
+    // void inorder(TreeNode* root,int k,int &ans){
+    //     if(!root || count>=k) return;
+    //     inorder(root->left,k,ans);
+    //     count++;
+    //     if(count==k){
+    //         ans=root->val;
+    //         return;
+    //     }
+    //     inorder(root->right,k,ans);
+    // }
     int kthSmallest(TreeNode* root, int k) {
         int ans;
-        inorder(root,k,ans);
+        // inorder(root,k,ans);
+        // return ans;
+
+        //morris approach for O(1) space 
+        TreeNode* curr=root;
+        while(curr){
+            if(!curr->left){
+                count++;
+                if(count==k){
+                    ans=curr->val;
+                }
+                curr=curr->right;
+            }
+            else{
+                TreeNode* prev=curr->left;
+                while(prev->right && prev->right!=curr){
+                    prev=prev->right;
+                }
+                if(prev->right==nullptr){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=nullptr;
+                    count++;
+                    if(count==k){
+                        ans=curr->val;
+                    }
+                    curr=curr->right;
+                }
+            }
+        }
         return ans;
     }
 };
