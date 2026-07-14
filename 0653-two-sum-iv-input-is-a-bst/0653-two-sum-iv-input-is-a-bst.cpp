bbda078b608683fad43a8114fe805d079703b02a
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-14 19:22:29 +0530

    Time: 3 ms (52.13%), Space: 37.3 MB (64.01%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,21 +11,49 @@
  */
 class Solution {
 public:
-    unordered_set<int> st;
-    int ans=0;
-    void inorder(TreeNode* root,int k){
-        if(!root || ans) return;
-        inorder(root->left,k);
-        // int more=k-root->val;
-        if(st.count(k-root->val)){
-            ans=1;
-            return;
+    stack<TreeNode*> st1;
+    stack<TreeNode*> st2;
+
+    void small(TreeNode* root){
+        while(root){
+            st1.push(root);
+            root=root->left;
         }
-        st.insert(root->val);
-        inorder(root->right,k);
     }
+     void greater(TreeNode* root){
+        while(root){
+            st2.push(root);
+            root=root->right;
+        }
+    }
+    int nextSmaller(){
+        TreeNode* el=st1.top();
+        st1.pop();
+        if(el->right){
+            small(el->right);
+        }
+        return el->val;
+    }
+
+    int nextGreater(){
+        TreeNode* el=st2.top();
+        st2.pop();
+        if(el->left){
+            greater(el->left);
+        }
+        return el->val;
+    }
+
     bool findTarget(TreeNode* root, int k) {
-        inorder(root,k);
-        return ans;
+        small(root);
+        greater(root);
+        int left=nextSmaller();
+        int right=nextGreater();
+        while(left<right){
+            if(left+right==k) return true;
+            else if(left+right>k) right=nextGreater();
+            else left=nextSmaller();
+        }
+        return false;
     }
 };
