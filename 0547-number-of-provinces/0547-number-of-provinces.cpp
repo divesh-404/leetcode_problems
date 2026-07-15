980dffbdb1496a2dfe73b41d74e98bf174a4396f
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-15 19:41:54 +0530

    Time: 0 ms (100%), Space: 19.5 MB (72.53%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,53 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& adjLs,int node,vector<int>& vis){
+    // void dfs(vector<vector<int>>& adjLs,int node,vector<int>& vis){
+    //     vis[node]=1;
+    //     for(auto it: adjLs[node]){
+    //         if(!vis[it]) dfs(adjLs,it,vis);
+    //     }
+    // }
+
+    void dfs(vector<vector<int>>& isConnected,int node,vector<int>& vis){
         vis[node]=1;
-        for(auto it: adjLs[node]){
-            if(!vis[it]) dfs(adjLs,it,vis);
+        int n=isConnected.size();
+        for(int j=0;j<n;j++){
+            if(isConnected[node][j]==1 && !vis[j]){
+                dfs(isConnected,j,vis);
+            }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected[0].size();
-        vector<vector<int>> adjLs(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1 && i!=j){
-                    adjLs[i].push_back(j);
-                    adjLs[j].push_back(i);
+        // int n=isConnected[0].size();
+        // vector<vector<int>> adjLs(n);
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(isConnected[i][j]==1 && i!=j){
+        //             adjLs[i].push_back(j);
+        //             adjLs[j].push_back(i);
                     
-                }
-            }
-        }
+        //         }
+        //     }
+        // }
 
+        // vector<int> vis(n,0);
+        // int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     if(!vis[i]){
+        //         cnt++;
+        //         dfs(adjLs,i,vis);
+        //     }
+        // }
+        // return cnt;
+
+        //direct from matrix apporach
+
+        int n=isConnected.size();
         vector<int> vis(n,0);
         int cnt=0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 cnt++;
-                dfs(adjLs,i,vis);
+                dfs(isConnected,i,vis);
             }
         }
         return cnt;
