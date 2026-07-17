00faabf88507f4feac9527d1b8a3148c3e33abd0
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-18 01:36:48 +0530

    Time: 30 ms (34.3%), Space: 24.3 MB (17.91%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dirRow[4]={-1,0,1,0};
+    int dirCol[4]={0,1,0,-1};
+    void bfs(int i,int j,vector<vector<int>>& vis,vector<vector<char>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+        vis[i][j]=1;
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        while(!q.empty()){
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int row=r+dirRow[i];
+                int col=c+dirCol[i];
+                if(row>=0 && row<n && col>=0 && col<m && !vis[row][col] && grid[row][col]=='1'){
+                    vis[row][col]=1;
+                    q.push({row,col});
+                }
+            }
+        }
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+       
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]) continue;
+                if(grid[i][j]=='1'){
+                    bfs(i,j,vis,grid);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
