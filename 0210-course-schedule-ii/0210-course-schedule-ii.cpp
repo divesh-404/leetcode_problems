d3c3783dadbce13b3617cfdac00f09827022b02c
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-18 03:22:39 +0530

    Time: 7 ms (32.87%), Space: 19.3 MB (11.65%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool dfsCycle(vector<int>& vis,vector<int>& pathVis,int node,vector<vector<int>>& adjLs,vector<int>& ans){
+        vis[node]=1;
+        pathVis[node]=1;
+        for(auto it: adjLs[node]){
+            if(!vis[it]){
+                if(dfsCycle(vis,pathVis,it,adjLs,ans)) return true;
+            }
+            else if(pathVis[it]) return true;
+        }
+        pathVis[node]=0;
+        ans.push_back(node);
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjLs(numCourses);
+        for(auto it: prerequisites){
+            adjLs[it[1]].push_back(it[0]);
+        }
+        vector<int> ans;
+        vector<int> vis(numCourses,0);
+        vector<int> pathVis(numCourses,0);
+
+        for(int i=0;i<numCourses;i++){
+            if(vis[i]) continue;
+            if(dfsCycle(vis,pathVis,i,adjLs,ans)) return {};
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
