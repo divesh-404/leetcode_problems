71871c5332bd0d4e9823803e9bc27b860bcb159a
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-18 01:38:17 +0530

    Time: 29 ms (42.24%), Space: 24.4 MB (17.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -30,8 +30,7 @@ public:
         int count=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]) continue;
-                if(grid[i][j]=='1'){
+                if(!vis[i][j] && grid[i][j]=='1'){
                     bfs(i,j,vis,grid);
                     count++;
                 }
