9583ac643c78c441829b5b1d8173dfd4e7f524b6
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-18 17:02:03 +0530

    Time: 6 ms (38.08%), Space: 19 MB (19.87%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
-    bool dfsCycle(vector<int>& vis,vector<int>& pathVis,int node,vector<vector<int>>& adjLs,vector<int>& ans){
-        vis[node]=1;
-        pathVis[node]=1;
-        for(auto it: adjLs[node]){
-            if(!vis[it]){
-                if(dfsCycle(vis,pathVis,it,adjLs,ans)) return true;
-            }
-            else if(pathVis[it]) return true;
-        }
-        pathVis[node]=0;
-        ans.push_back(node);
-        return false;
-    }
+    // bool dfsCycle(vector<int>& vis,vector<int>& pathVis,int node,vector<vector<int>>& adjLs,vector<int>& ans){
+    //     vis[node]=1;
+    //     pathVis[node]=1;
+    //     for(auto it: adjLs[node]){
+    //         if(!vis[it]){
+    //             if(dfsCycle(vis,pathVis,it,adjLs,ans)) return true;
+    //         }
+    //         else if(pathVis[it]) return true;
+    //     }
+    //     pathVis[node]=0;
+    //     ans.push_back(node);
+    //     return false;
+    // }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adjLs(numCourses);
         for(auto it: prerequisites){
             adjLs[it[1]].push_back(it[0]);
         }
         vector<int> ans;
-        vector<int> vis(numCourses,0);
-        vector<int> pathVis(numCourses,0);
+        // vector<int> vis(numCourses,0);
+        // vector<int> pathVis(numCourses,0);
 
+        // for(int i=0;i<numCourses;i++){
+        //     if(vis[i]) continue;
+        //     if(dfsCycle(vis,pathVis,i,adjLs,ans)) return {};
+        // }
+        // reverse(ans.begin(),ans.end());
+        // return ans;
+
+        vector<int> indegree(numCourses,0);
         for(int i=0;i<numCourses;i++){
-            if(vis[i]) continue;
-            if(dfsCycle(vis,pathVis,i,adjLs,ans)) return {};
+            for(auto it: adjLs[i]){
+                indegree[it]++;
+            }
         }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        queue<int> q;
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
+        }
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it: adjLs[node]){
+                indegree[it]--;
+                if(indegree[it]==0) q.push(it);
+            }
+        }
+
+        if(ans.size()==numCourses) return ans;
+        return {};
     }
 };
