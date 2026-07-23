9ad4681a73d0cf029c1ebf5a463ad28018786a0e
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-23 16:34:29 +0530

    Time: 15 ms (60.01%), Space: 46.7 MB (50.57%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,26 +1,68 @@
 class Solution {
 public:
-    void dfs(int node,vector<int>& vis,vector<vector<int>>& adj){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]) dfs(it,vis,adj);
+class DSU{
+public:
+    vector<int> size,parent;
+    DSU(int n){
+        size.resize(n+1,1);
+        parent.resize(n+1,1);
+        for(int i=0;i<=n;i++) parent[i]=i;
+    }
+
+    int findPar(int node){
+        if(node==parent[node]) return node;
+        return parent[node]=findPar(parent[node]);
+    }
+
+    void unionBySize(int u,int v){
+        int ulp_u=findPar(u);
+        int ulp_v=findPar(v);
+        if(ulp_u==ulp_v) return;
+        if(size[ulp_u]<size[ulp_v]){
+            parent[ulp_u]=ulp_v;
+            size[ulp_v]+=size[ulp_u];
+        }
+        else {
+            parent[ulp_v]=ulp_u;
+            size[ulp_u]+=size[ulp_v];
         }
     }
+};
+    // void dfs(int node,vector<int>& vis,vector<vector<int>>& adj){
+    //     vis[node]=1;
+    //     for(auto it:adj[node]){
+    //         if(!vis[it]) dfs(it,vis,adj);
+    //     }
+    // }
     int makeConnected(int n, vector<vector<int>>& connections) {
         if(connections.size()<(n-1)) return -1;
-        vector<vector<int>> adj(n);
+        // vector<vector<int>> adj(n);
+        // for(auto it: connections){
+        //     adj[it[0]].push_back(it[1]);
+        //     adj[it[1]].push_back(it[0]);
+        // }
+        // vector<int> vis(n,0);
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //     if(vis[i]) continue;
+        //     count++;
+        //     dfs(i,vis,adj);
+        // }
+
+        // return count-1;
+
+        // by disjoint set method
+
+        DSU ds(n);
         for(auto it: connections){
-            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+            ds.unionBySize(it[0],it[1]);
         }
-        vector<int> vis(n,0);
-        int count=0;
+        int components=0;
         for(int i=0;i<n;i++){
-            if(vis[i]) continue;
-            count++;
-            dfs(i,vis,adj);
+            if(ds.findPar(i)==i) components++;
         }
 
-        return count-1;
+        return components-1;
+
     }
 };
