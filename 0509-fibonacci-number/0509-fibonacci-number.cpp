c6ecbdc49626fc929e05bb487de96f806ebc7e1c
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-28 03:09:45 +0530

    Time: 2 ms (78.02%), Space: 7.8 MB (65.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -24,13 +24,13 @@ public:
         if(n<=1) return n;
         int prev2=0;
         int prev1=1;
-        int curr;
+        // int curr;
         for(int i=2;i<=n;i++){
-            curr=prev1+prev2;
+            int curr=prev1+prev2;
             prev2=prev1;
             prev1=curr;
         }
 
-        return curr;
+        return prev1;
     }
 };
