f1d3f0605a655b12f0a41720d765da9b39638a00
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-28 03:09:11 +0530

    Time: 2 ms (78.02%), Space: 7.9 MB (38.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
+    // memoization
     int fun(int n,vector<int>& dp){
         if(n<=1) return n;
         if(dp[n]!=-1) return dp[n];
         return dp[n]=fun(n-1,dp)+fun(n-2,dp);
     }
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return fun(n,dp);
+        // vector<int> dp(n+1,-1);
+        // return fun(n,dp);
         
+        // tabulation with space
         // if(n<=1) return n;
         // dp[0]=0;
         // dp[1]=1;
         // for(int i=2;i<=n;i++){
         //     dp[i]=dp[i-1]+dp[i-2];
         // }
-
         // return dp[n];
+
+        // tabulation without extra space
+
+        if(n<=1) return n;
+        int prev2=0;
+        int prev1=1;
+        int curr;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+        }
+
+        return curr;
     }
 };
