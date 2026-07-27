b40f6ad6a990f9528659fe9f998f96f2ae0fa5ae
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-28 03:09:24 +0530

    Time: 2 ms (78.02%), Space: 7.9 MB (38.71%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     // memoization
-    int fun(int n,vector<int>& dp){
-        if(n<=1) return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=fun(n-1,dp)+fun(n-2,dp);
-    }
+    // int fun(int n,vector<int>& dp){
+    //     if(n<=1) return n;
+    //     if(dp[n]!=-1) return dp[n];
+    //     return dp[n]=fun(n-1,dp)+fun(n-2,dp);
+    // }
     int fib(int n) {
         // vector<int> dp(n+1,-1);
         // return fun(n,dp);
