599d00057c8566ea185559759dafb7c971a3fb83
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-29 12:50:29 +0530

    Time: 0 ms (100%), Space: 10 MB (97.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -16,16 +16,31 @@ public:
         // return fun(n-1,dp,nums);
         
         //tabulation
-        vector<int> dp(n,0);
-        dp[0]=nums[0];
+        // vector<int> dp(n,0);
+        // dp[0]=nums[0];
+        // for(int i=1;i<n;i++){
+        //     int take=nums[i];
+        //     if(i>1) take+=dp[i-2];
+        //     int nottake=dp[i-1];
+        //     dp[i]=max(take,nottake);
+        // }
+
+        // return dp[n-1];
+
+        //space optimization;
+        int prev1=nums[0];
+        int prev2=0;
         for(int i=1;i<n;i++){
             int take=nums[i];
-            if(i>1) take+=dp[i-2];
-            int nottake=dp[i-1];
-            dp[i]=max(take,nottake);
+            if(i>1) take+=prev2;
+            int nottake=prev1;
+
+            int curr=max(take,nottake);
+            prev2=prev1;
+            prev1=curr;
         }
 
-        return dp[n-1];
+        return prev1;
 
     }
 };
