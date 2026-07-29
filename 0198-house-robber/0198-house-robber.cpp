6eb2bde23ebe99504e42edbf0e934ab83612b535
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-29 12:48:18 +0530

    Time: 3 ms (6.03%), Space: 10.8 MB (24.29%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,16 +1,31 @@
 class Solution {
 public:
-    int fun(int ind,vector<int>& dp,vector<int>& nums){
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]!=-1) return dp[ind];
-        int pick=nums[ind]+fun(ind-2,dp,nums);
-        int notpick=fun(ind-1,dp,nums);
-        return dp[ind]=max(pick,notpick);
-    }
+    // memoization
+    // int fun(int ind,vector<int>& dp,vector<int>& nums){
+    //     if(ind==0) return nums[ind];
+    //     if(ind<0) return 0;
+    //     if(dp[ind]!=-1) return dp[ind];
+    //     int pick=nums[ind]+fun(ind-2,dp,nums);
+    //     int notpick=fun(ind-1,dp,nums);
+    //     return dp[ind]=max(pick,notpick);
+    // }
     int rob(vector<int>& nums) {
-        int ind=nums.size()-1;
-        vector<int> dp(ind+1,-1);
-        return fun(ind,dp,nums);
+        int n=nums.size();
+        //memoization
+        // vector<int> dp(n,-1);
+        // return fun(n-1,dp,nums);
+        
+        //tabulation
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+            int nottake=dp[i-1];
+            dp[i]=max(take,nottake);
+        }
+
+        return dp[n-1];
+
     }
 };
