ff8820abb14f36ecb35ae4d0423b104e60ca5c89
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-30 12:49:22 +0530

    Time: 0 ms (100%), Space: 8.4 MB (77.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -26,22 +26,43 @@ public:
         // return fun(m-1,n-1,dp);
 
         //tabulation
-        vector<vector<int>> dp(m,vector<int>(n,0)); 
-        dp[0][0]=1;
+        // vector<vector<int>> dp(m,vector<int>(n,0)); 
+        // dp[0][0]=1;
+
+        // for(int row=0;row<m;row++){
+        //     for(int col=0;col<n;col++){
+        //         if(row==0 && col==0) continue;
+        //         int left=0;
+        //         if(col>0) left=dp[row][col-1];
+        //         int up=0;
+        //         if(row>0) up=dp[row-1][col];
+
+        //         dp[row][col]=left+up;
+        //     }
+        // }
+
+        // return dp[m-1][n-1];
+
+        // space optimization of tabulation
+
+        vector<int> prev(n,0);
 
         for(int row=0;row<m;row++){
+            vector<int> temp(n);
             for(int col=0;col<n;col++){
-                if(row==0 && col==0) continue;
+                if(row==0 && col==0){
+                    temp[0]=1;
+                    continue;
+                }
                 int left=0;
-                if(col>0) left=dp[row][col-1];
-                int up=0;
-                if(row>0) up=dp[row-1][col];
-
-                dp[row][col]=left+up;
+                if(col>0) left=temp[col-1];
+                int up=prev[col];
+                temp[col]=up+left;
             }
+            prev=temp;
         }
 
-        return dp[m-1][n-1];
+        return prev[n-1];
 
         
     }
