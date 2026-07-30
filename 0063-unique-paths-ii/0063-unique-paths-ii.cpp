7242cdc035b1825fb9603fd2d1c71ac8bcbb33db
Divesh Kumavat <diveshkumavat@gmail.com>
2026-07-30 22:44:44 +0530

    Time: 0 ms (100%), Space: 11.6 MB (85.75%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int fun(int r,int c, vector<vector<int>>& dp,vector<vector<int>>& obstacleGrid){
         if(r<0 || c<0) return 0;
-        if(obstacleGrid[r][c]==1) return dp[r][c]==0;
+        if(obstacleGrid[r][c]==1) return dp[r][c]=0;
         if(dp[r][c]!=-1) return dp[r][c];
         if(r==0 and c==0) return dp[r][c]=1;
 
@@ -22,49 +22,49 @@ public:
         if(obstacleGrid[0][0]==1) return 0;
 
         //tabulation
-        vector<vector<int>> dp(m,vector<int>(n,0));
-        dp[0][0]=1;
+        // vector<vector<int>> dp(m,vector<int>(n,0));
+        // dp[0][0]=1;
 
-        for(int r=0;r<m;r++){
-            for(int c=0;c<n;c++){
-                if(r==0 && c==0) continue;
-                if(obstacleGrid[r][c]==1){
-                    dp[r][c]=0;
-                    continue;
-                }
-                int left=0;
-                if(c>0) left=dp[r][c-1];
-                int up=0;
-                if(r>0) up=dp[r-1][c];
-
-                dp[r][c]=left+up;
-            }
-        }
-
-        return dp[m-1][n-1];
-
-        // space optimization
-
-        // vector<int> prev(n,0);
         // for(int r=0;r<m;r++){
-        //     vector<int> temp(n);
         //     for(int c=0;c<n;c++){
-        //         if(r==0 && c==0){
-        //             temp[0]=1;
-        //             continue;
-        //         }
+        //         if(r==0 && c==0) continue;
         //         if(obstacleGrid[r][c]==1){
-        //             temp[c]=0;
+        //             dp[r][c]=0;
         //             continue;
         //         }
         //         int left=0;
-        //         if(c>0) left=temp[c-1];
-        //         int up=temp[c];
-        //         temp[c]= left+up;
+        //         if(c>0) left=dp[r][c-1];
+        //         int up=0;
+        //         if(r>0) up=dp[r-1][c];
+
+        //         dp[r][c]=left+up;
         //     }
-        //     prev=temp;
         // }
 
-        // return prev[n-1];
+        // return dp[m-1][n-1];
+
+        // space optimization
+
+        vector<int> prev(n,0);
+        for(int r=0;r<m;r++){
+            vector<int> temp(n);
+            for(int c=0;c<n;c++){
+                if(r==0 && c==0){
+                    temp[0]=1;
+                    continue;
+                }
+                if(obstacleGrid[r][c]==1){
+                    temp[c]=0;
+                    continue;
+                }
+                int left=0;
+                if(c>0) left=temp[c-1];
+                int up=prev[c];
+                temp[c]= left+up;
+            }
+            prev=temp;
+        }
+
+        return prev[n-1];
     }
 };
