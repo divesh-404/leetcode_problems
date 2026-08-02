b083aa26cd52cb863adc0efc299d35c359e3f3de
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-03 02:33:59 +0530

    Time: 1 ms (89.85%), Space: 12.8 MB (62.7%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -13,11 +13,36 @@ public:
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
+
+        // map<pair<int,int>,int> mp;
+        // return fun(n-1,target,nums,mp);
+
+        // other method s1-s2 =target
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=nums[i];
         }
-        map<pair<int,int>,int> mp;
-        return fun(n-1,target,nums,mp);
+        int subset=(sum-target)/2;
+        if(sum-target<0) return 0;
+        if((sum-target)%2!=0) return 0;
+
+        vector<int> prev(subset+1,0) , curr(subset+1,0);
+
+        if(nums[0]==0) prev[0]=2;
+        else prev[0]=1;
+
+        if(nums[0]!=0 && nums[0]<=subset) prev[nums[0]]=1;
+
+        for(int i=1;i<n;i++){
+            for(int tar=0;tar<=subset;tar++){
+                int notTake=prev[tar];
+                int take=0;
+                if(nums[i]<=tar) take=prev[tar-nums[i]];
+                curr[tar]=take+notTake;
+            }
+            prev=curr;
+        }
+        return prev[subset];
+
     }
 };
