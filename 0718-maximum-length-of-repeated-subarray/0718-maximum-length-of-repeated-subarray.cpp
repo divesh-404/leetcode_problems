778757b7fbbee6f3df37dd446dd5d29b878d91a7
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-05 02:44:39 +0530

    Time: 45 ms (89.42%), Space: 14.7 MB (90.79%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -3,17 +3,33 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n=nums1.size();
         int m=nums2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        //tabulation
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        //base case
-        // if(nums1[0]==nums2[0]) dp[1][1]=1;
+        // //base case
+        // // if(nums1[0]==nums2[0]) dp[1][1]=1;
+        // int len=0;
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+        //         else dp[i][j]=0;
+        //         len=max(len,dp[i][j]);
+        //     }
+        // }
+
+        // return len;
+
+        //space optimization
+
+        vector<int> prev(m+1,0),curr(m+1,0); //coloums
         int len=0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(nums1[i-1]==nums2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else dp[i][j]=0;
-                len=max(len,dp[i][j]);
+                if(nums1[i-1]==nums2[j-1]) curr[j]=1+prev[j-1];
+                else curr[j]=0;
+                len=max(len,curr[j]);
             }
+            prev=curr;
         }
 
         return len;
