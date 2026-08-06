cf302ab56ecef224fbb0fcbc9f187222edefb4ac
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-06 18:12:37 +0530

    Time: 54 ms (49.02%), Space: 54.7 MB (18.22%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -24,16 +24,35 @@ public:
 
         return dp[i][j]=false;
 
-
-
-
     }
     bool isMatch(string s, string p) {
         int n=s.size();
         int m=p.size();
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
 
-        return fun(n,m,s,p,dp);
+        // return fun(n,m,s,p,dp);
+
+        // tabulation
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        dp[0][0]=1;
+        for(int i=1;i<=n;i++){
+            dp[i][0]=0;
+        }
+
+        for(int j=1;j<=m;j++){
+           dp[0][j]=dp[0][j-1] && p[j-1]=='*';
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
+                if(p[j-1]=='*') dp[i][j]=dp[i][j-1] || dp[i-1][j];
+            }
+        }
+
+        return dp[n][m];
     }
 };
