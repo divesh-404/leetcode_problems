8d83c03fc0ca93c6f7662c0cd5052544a001e978
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-06 18:21:42 +0530

    Time: 57 ms (41.53%), Space: 52.5 MB (43.63%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -35,24 +35,44 @@ public:
 
         // tabulation
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        dp[0][0]=1;
-        for(int i=1;i<=n;i++){
-            dp[i][0]=0;
-        }
+        // dp[0][0]=1;
+        // for(int i=1;i<=n;i++){
+        //     dp[i][0]=0;
+        // }
 
+        // for(int j=1;j<=m;j++){
+        //    dp[0][j]=dp[0][j-1] && p[j-1]=='*';
+        // }
+
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=m;j++){
+        //         if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
+        //         if(p[j-1]=='*') dp[i][j]=dp[i][j-1] || dp[i-1][j];
+        //     }
+        // }
+
+        // return dp[n][m];
+
+        //space optimization
+
+        vector<int> prev(m+1,0);
+
+        prev[0]=1;
         for(int j=1;j<=m;j++){
-           dp[0][j]=dp[0][j-1] && p[j-1]=='*';
+            prev[j]=prev[j-1] && p[j-1]=='*';
         }
 
         for(int i=1;i<=n;i++){
+            vector<int> curr(m+1,0);
             for(int j=1;j<=m;j++){
-                if(s[i-1]==p[j-1] || p[j-1]=='?') dp[i][j]=dp[i-1][j-1];
-                if(p[j-1]=='*') dp[i][j]=dp[i][j-1] || dp[i-1][j];
+                if(s[i-1]==p[j-1] || p[j-1]=='?') curr[j]=prev[j-1];
+                if(p[j-1]=='*') curr[j]=prev[j] || curr[j-1];
             }
+            prev=curr;
         }
 
-        return dp[n][m];
+        return prev[m];
     }
 };
