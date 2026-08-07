8aa749756c3ef81dffaff0fc74bbacf5a48e0147
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-07 18:49:19 +0530

    Time: 149 ms (38.73%), Space: 14.5 MB (48.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -18,22 +18,37 @@ public:
         // return fun(0,-1,n,nums,dp);
 
         //tabulation
-        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
-        for(int j=0;j<=n;j++){
-            dp[n][j]=0;
-        }
+        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // for(int j=0;j<=n;j++){
+        //     dp[n][j]=0;
+        // }
+
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=i-1;j>=-1;j--){
+        //         int notTake=0+dp[i+1][j+1];
+        //         int take=0;
+        //         if(j==-1 || nums[i]>nums[j]) take=1+dp[i+1][i+1];
+
+        //         dp[i][j+1]=max(take,notTake);
+        //     }
+        // }
+
+        // return dp[0][0];
+
+        //space optimization
+
+        vector<int> after(n+1,0),curr(n+1,0);
 
         for(int i=n-1;i>=0;i--){
             for(int j=i-1;j>=-1;j--){
-                int notTake=0+dp[i+1][j+1];
+                int notTake=0+after[j+1];
                 int take=0;
-                if(j==-1 || nums[i]>nums[j]) take=1+dp[i+1][i+1];
+                if(j==-1 || nums[i]>nums[j]) take=1+after[i+1];
 
-                dp[i][j+1]=max(take,notTake);
+                curr[j+1]=max(take,notTake);
             }
+            after=curr;
         }
-
-        return dp[0][0];
-
+        return after[0];
     }
 };
