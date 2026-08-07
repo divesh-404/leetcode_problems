bdfae186e267ebc1f6f8b61e74192573d45491b4
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-08 02:21:44 +0530

    Time: 0 ms (100%), Space: 14.1 MB (77.93%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -71,10 +71,11 @@ public:
 
         vector<int> temp;
         temp.push_back(nums[0]);
-
+        int len=1;
         for(int i=1;i<n;i++){
             if(nums[i]>temp.back()){
                 temp.push_back(nums[i]);
+                len++;
             }
             else{
                 int ind=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
@@ -82,6 +83,6 @@ public:
             }
         }
 
-        return temp.size();
+        return len;
     }
 };
