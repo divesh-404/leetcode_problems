07ba904c94037abac57708cbd8f856dd45e4617c
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-11 00:41:01 +0530

    Time: 105 ms (65.8%), Space: 13.9 MB (62.27%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -15,24 +15,24 @@ public:
         nums.insert(nums.begin(),1);
         nums.push_back(1);
         int n=nums.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        return fun(1,n-2,nums,dp);
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        // return fun(1,n-2,nums,dp);
 
 
         //tabulation
-        // vector<vector<int>> dp(n,vector<int>(n,0));
+        vector<vector<int>> dp(n,vector<int>(n,0));
 
-        // for(int i=n-2;i>=1;i--){
-        //     for(int j=i;j<=n-2;j++){
-        //         int maxi=-1e9;
-        //         for(int k=i;k<=j;k++){
-        //             int amount=nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1]+dp[k+1][j];
-        //             maxi=max(amount,maxi);
-        //         }
-        //         dp[i][j]=maxi;
-        //     }
-        // }
+        for(int i=n-2;i>=1;i--){
+            for(int j=i;j<=n-2;j++){
+                int maxi=-1e9;
+                for(int k=i;k<=j;k++){
+                    int amount=nums[i-1]*nums[k]*nums[j+1] + dp[i][k-1]+dp[k+1][j];
+                    maxi=max(amount,maxi);
+                }
+                dp[i][j]=maxi;
+            }
+        }
 
-        // return dp[1][n-2];
+        return dp[1][n-2];
     }
 };
