7e89474f59535c645d02041ec786e2a67509668e
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-11 19:35:42 +0530

    Time: 52 ms (73.52%), Space: 62.9 MB (8.82%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -21,7 +21,21 @@ public:
             }
         }
 
-        vector<int> dp(n,-1);
-        return fun(0,s,dp,isPal)-1;
+        // vector<int> dp(n,-1);
+        // return fun(0,s,dp,isPal)-1;
+
+        //tabulation
+
+        vector<int> dp(n+1,0);
+
+        for(int i=n-1;i>=0;i--){
+            int mini=1e9;
+            for(int j=i;j<n;j++){
+                if(isPal[i][j]) mini=min(mini,1+dp[j+1]);
+            }
+            dp[i]=mini;
+        }
+
+        return dp[0]-1;
     }
 };
