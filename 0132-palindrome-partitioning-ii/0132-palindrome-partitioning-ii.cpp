f4be547436fcc6237b3c19b779e0ddeb1c1849e6
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-11 19:32:39 +0530

    Time: 67 ms (63.55%), Space: 62.9 MB (8.82%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,45 +1,27 @@
 class Solution {
 public:
-    int fun(int i, string& s,vector<vector<int>>& isPal,vector<int>& dp){
+    int fun(int i,string& s,vector<int>& dp,vector<vector<int>>& isPal){
         int n=s.size();
-        if(i==n) return -1;
+        if(i==n) return 0;
         if(dp[i]!=-1) return dp[i];
 
         int mini=1e9;
         for(int j=i;j<n;j++){
-            if(isPal[i][j]){
-                mini=min(mini,1+fun(j+1,s,isPal,dp));
-            }
+            if(isPal[i][j]) mini=min(mini,1+fun(j+1,s,dp,isPal));
         }
+
         return dp[i]=mini;
     }
     int minCut(string s) {
         int n=s.size();
         vector<vector<int>> isPal(n,vector<int>(n,0));
-
         for(int i=n-1;i>=0;i--){
             for(int j=i;j<n;j++){
                 if(s[i]==s[j] && (j-i<2 || isPal[i+1][j-1])) isPal[i][j]=1;
             }
         }
 
-        // vector<int> dp(n,-1);
-        // return fun(0,s,isPal,dp);
-
-        //tabulation
-
-        vector<int> dp(n+1,0);
-        dp[n]=-1;
-
-
-        for(int i=n-1;i>=0;i--){
-            int mini=1e9;
-            for(int j=i;j<n;j++){
-                if(isPal[i][j]) mini=min(mini,1+dp[j+1]);
-            }
-            dp[i]=mini;
-        }
-
-        return dp[0];
+        vector<int> dp(n,-1);
+        return fun(0,s,dp,isPal)-1;
     }
 };
