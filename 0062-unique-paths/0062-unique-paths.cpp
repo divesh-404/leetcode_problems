60ece4c4fdf83b61a0c4c1fb25a005ce66d36d8f
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-14 17:36:54 +0530

    Time: 2 ms (20.86%), Space: 8.3 MB (77.92%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,69 +1,56 @@
 class Solution {
 public:
-    // memoization
-    int fun(int row,int col,vector<vector<int>>& dp){
-        if(row<0 || col<0) return 0;
-        if(dp[row][col]!=-1) return dp[row][col];
-        if(row==0 && col==0) return dp[row][col]=1;
+    //memoization
+    int fun(int i,int j,vector<vector<int>>& dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
 
-        int left=fun(row,col-1,dp);
-        int up=fun(row-1,col,dp);
+        int up=fun(i-1,j,dp);
+        int left=fun(i,j-1,dp);
 
-        return dp[row][col]=left + up;
-
+        return dp[i][j]=up+left;
     }
     int uniquePaths(int m, int n) {
-        // permutation method nCr
-        // long long ans=1;
-        // int N=(m-1)+(n-1);
-        // int R=min((m-1),(n-1));
-        // for(int i=1;i<=R;i++){
-        //     ans=ans*(N-R+i)/i;
-        // }
-        // return (int)ans;
-
         // vector<vector<int>> dp(m,vector<int>(n,-1));
         // return fun(m-1,n-1,dp);
 
         //tabulation
-        // vector<vector<int>> dp(m,vector<int>(n,0)); 
+        // vector<vector<int>> dp(m,vector<int>(n,0));
         // dp[0][0]=1;
 
-        // for(int row=0;row<m;row++){
-        //     for(int col=0;col<n;col++){
-        //         if(row==0 && col==0) continue;
-        //         int left=0;
-        //         if(col>0) left=dp[row][col-1];
-        //         int up=0;
-        //         if(row>0) up=dp[row-1][col];
-
-        //         dp[row][col]=left+up;
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i==0 && j==0) continue;
+        //         int right=0;
+        //         if(j>0) right=dp[i][j-1];
+        //         int down=0;
+        //         if(i>0) down=dp[i-1][j];
+        //         dp[i][j]=right+down;
         //     }
         // }
 
         // return dp[m-1][n-1];
 
-        // space optimization of tabulation
-
+        //space optimization
         vector<int> prev(n,0);
-
-        for(int row=0;row<m;row++){
-            vector<int> temp(n);
-            for(int col=0;col<n;col++){
-                if(row==0 && col==0){
-                    temp[0]=1;
+        for(int i=0;i<m;i++){
+            vector<int> curr(n,0);
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    curr[0]=1;
                     continue;
                 }
-                int left=0;
-                if(col>0) left=temp[col-1];
-                int up=prev[col];
-                temp[col]=up+left;
+                int right=0;
+                if(j>0) right=curr[j-1];
+                int down=0;
+                if(i>0) down=prev[j];
+                curr[j]=right+down;
             }
-            prev=temp;
+            prev=curr;
         }
 
         return prev[n-1];
 
-        
     }
 };
