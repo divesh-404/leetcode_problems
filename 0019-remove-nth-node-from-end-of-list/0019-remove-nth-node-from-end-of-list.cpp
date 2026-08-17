4072389229f8a8108e3b1d5a78defe6662f75801
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-17 17:58:54 +0530

    Time: 0 ms (100%), Space: 14.9 MB (60.03%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -11,28 +11,51 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* mover=head;
-        // if(n==1) return NULL;
-        int size=0;
-        while(mover!=nullptr){
-            size++;
-            mover=mover->next;
-        }
-        if(n==size){
-           ListNode* temp=head;
-           head=head->next;
-           delete temp;
-           return head;
-        }
-        ListNode* curr=head;
-        ListNode* prev=nullptr;
-        for(int i=0;i<size-n;i++){
-            prev=curr;
-            curr=curr->next;
-        }
-        prev->next=curr->next;
-        delete(curr);
+        // ListNode* mover=head;
+        // int size=0;
+        // while(mover!=nullptr){
+        //     size++;
+        //     mover=mover->next;
+        // }
+        // if(n==size){
+        //    ListNode* temp=head;
+        //    head=head->next;
+        //    delete temp;
+        //    return head;
+        // }
+        // ListNode* curr=head;
+        // ListNode* prev=nullptr;
+        // for(int i=0;i<size-n;i++){
+        //     prev=curr;
+        //     curr=curr->next;
+        // }
+        // prev->next=curr->next;
+        // delete(curr);
 
-        return head;
+        // return head;
+
+        ListNode* dummy=new ListNode(-1);
+        dummy->next=head;
+
+        ListNode* first=dummy;
+        ListNode* second=dummy;
+
+        while(n>=0){
+            n--;
+            first=first->next;
+        }
+
+        while(first!=nullptr){
+            first=first->next;
+            second=second->next;
+        }
+
+        ListNode* temp=second->next;
+        second->next=temp->next;
+        delete temp;
+
+        ListNode* ans=dummy->next;
+        delete dummy;
+        return ans;
     }
 };
