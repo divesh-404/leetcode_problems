0ef0acd61bc23cffef9ee2ab1dd412870eefe0d8
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-19 19:31:22 +0530

    Time: 11 ms (39.34%), Space: 44.8 MB (78.52%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int cnt=0;
+        // int cnt=0;
         sort(g.begin(),g.end());
         sort(s.begin(),s.end());
         int l=0;
@@ -9,11 +9,11 @@ public:
         while(l<g.size() && r<s.size()){
             if(g[l]<=s[r]){
                 l++;
-                cnt++;
+                // cnt++;
             }
             r++;
         }
 
-        return cnt;
+        return l;
     }
 };
