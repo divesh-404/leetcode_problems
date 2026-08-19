4c2d21ff4bcdb4585f1e4347312464dd7c5363cb
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-19 19:03:40 +0530

    Time: 47 ms (35.54%), Space: 49.5 MB (24.59%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,65 +1,23 @@
 class Solution {
 public:
-    int fun(int ind,int tar,vector<int>& coins,vector<vector<int>>& dp){
-        if(ind==0){
-            if(tar%coins[ind]==0) return tar/coins[ind];
+    int fun(int i,int target,vector<int>& coins,vector<vector<int>>& dp){
+        //base cases
+        if(i==0){
+            if(target%coins[i]==0) return target/coins[i];
             else return 1e9;
         }
-        if(dp[ind][tar]!=-1) return dp[ind][tar];
+        if(dp[i][target]!=-1) return dp[i][target];
 
-        int notTake= 0+fun(ind-1,tar,coins,dp);
+        int notTake=0+fun(i-1,target,coins,dp);
         int take=1e9;
-        if(coins[ind]<=tar) take=1+fun(ind,tar-coins[ind],coins,dp);
+        if(coins[i]<=target) take=1+fun(i,target-coins[i],coins,dp);
 
-        return dp[ind][tar]=min(take,notTake);
-
+        return dp[i][target]=min(take,notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-
-        // if(fun(n-1,amount,coins,dp)==1e9) return -1;
-        // return fun(n-1,amount,coins,dp);
-
-        //tabulation 
-        // vector<vector<int>> dp(n,vector<int>(amount+1,0));
-
-        // for(int tar=0;tar<=amount;tar++){
-        //     if(tar%coins[0]==0) dp[0][tar]=tar/coins[0];
-        //     else dp[0][tar]=1e9;
-        // }
-
-        // for(int i=1;i<n;i++){
-        //     for(int tar=0;tar<=amount;tar++){
-        //         int notTake=0+dp[i-1][tar];
-        //         int take=1e9;
-        //         if(coins[i]<=tar) take=1+dp[i][tar-coins[i]];
-        //         dp[i][tar]=min(take,notTake);
-        //     }
-        // }
-
-        // return dp[n-1][amount]>=1e9?-1:dp[n-1][amount];
-
-        //space optimization
-
-        vector<int> prev(amount+1,0);
-        for(int tar=0;tar<=amount;tar++){
-            if(tar%coins[0]==0) prev[tar]=tar/coins[0];
-            else prev[tar]=1e9;
-        }
-
-        for(int i=1;i<n;i++){
-            vector<int> curr(amount+1,0);
-            for(int tar=0;tar<=amount;tar++){
-                int notTake=prev[tar];
-                int take=1e9;
-                if(coins[i]<=tar) take=1+curr[tar-coins[i]];
-
-                curr[tar]=min(take,notTake);
-            }
-            prev=curr;
-        }
-
-        return prev[amount]>=1e9?-1:prev[amount];
+        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        if(fun(n-1,amount,coins,dp)==1e9) return -1;
+        return fun(n-1,amount,coins,dp);
     }
 };
