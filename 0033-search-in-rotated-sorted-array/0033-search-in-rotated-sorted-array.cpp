623f52c8f06a3656ed055b97d640312f17c58a02
Divesh Kumavat <diveshkumavat@gmail.com>
2026-08-20 15:28:43 +0530

    Time: 0 ms (100%), Space: 15.2 MB (69.01%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,20 +4,20 @@ public:
         int n=nums.size();
         int l=0;
         int h=n-1;
-
         while(l<=h){
             int m=l+(h-l)/2;
-
+            //checking which is sorted for both sides
             if(nums[m]==target) return m;
-
-            //if left half is sorted
             if(nums[l]<=nums[m]){
-                if(target>=nums[l] && target<nums[m]) h=m-1;
+                if(target>=nums[l] && target<=nums[m]){
+                    h=m;
+                }
                 else l=m+1;
             }
-            //if right half is sorted
             else{
-                if(target>nums[m] && target<=nums[h]) l=m+1;
+                if(target>=nums[m] && target<=nums[h]){
+                    l=m;
+                }
                 else h=m-1;
             }
         }
